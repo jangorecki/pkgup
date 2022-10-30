91070fde2a9933d47dadb6a4cd099c446c41b5f1
Jan Gorecki <J.Gorecki@wit.edu.pl>
2022-10-30 08:12:46 +0000

    amend tests after DCF url change

--- tests/tests.R
+++ tests/tests.R
@@ -4,10 +4,10 @@ cmd = installcmd()
 
 stopifnot(all.equal(
   unclass(cmd),
-  sprintf('install.packages("pkgup", repos=c("https://jangorecki.github.io/pkgup","%s"))', getOption("repos"))
+  sprintf('install.packages("pkgup", repos=c("https://jangorecki.github.io/pkgup/","%s"))', getOption("repos"))
 ))
 
 stopifnot(all.equal(
   capture.output(print(cmd)),
-  sprintf('install.packages("pkgup", repos=c("https://jangorecki.github.io/pkgup","%s"))', getOption("repos"))
+  sprintf('install.packages("pkgup", repos=c("https://jangorecki.github.io/pkgup/","%s"))', getOption("repos"))
 ))
